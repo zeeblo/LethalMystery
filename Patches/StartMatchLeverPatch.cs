32a7f4c1231167d69c087ee7017ff1f5a27fe8b4
syntax z <141220959+syntax-z@users.noreply.github.com>
2024-04-12 04:52:26 -0400

    not enough players
    Notify host that there isnt enough players when the user count is below 4.
    
    This also prevents the game from starting.
    (This is a cleaned up version of the previous commit)

--- Patches/StartMatchLeverPatch.cs
+++ Patches/StartMatchLeverPatch.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using UnityEngine;
 
 
 namespace LethalMystery.Patches
@@ -9,17 +10,13 @@ namespace LethalMystery.Patches
 
         [HarmonyPatch(typeof(StartMatchLever), nameof(StartMatchLever.BeginHoldingInteractOnLever))]
         [HarmonyPostfix]
-        private static void NotifyPatch()
+        private static void NotEnoughPlayers()
         {
             // Disable Lever if there's less than 4 players in lobby
             if (StartOfRound.Instance.ClientPlayerList.Keys.ToArray().Length < 4)
             {
-                float playerPosX = GameNetworkManager.Instance.localPlayerController.transform.position.x;
-                float playerPosY = GameNetworkManager.Instance.localPlayerController.transform.position.y;
-                float playerPosZ = GameNetworkManager.Instance.localPlayerController.transform.position.z;
-                UnityEngine.Vector3 playerPos = new UnityEngine.Vector3(playerPosX, playerPosY, playerPosZ-5);
-
-                GameNetworkManager.Instance.localPlayerController.TeleportPlayer(playerPos);
+                Transform[] playerSpawnPositions = StartOfRound.Instance.playerSpawnPositions;
+                GameNetworkManager.Instance.localPlayerController.TeleportPlayer(playerSpawnPositions[GameNetworkManager.Instance.localPlayerController.playerClientId].position);
                 HUDManager.Instance.DisplayTip("Not Enough Players!", "You need at least 4 players to start the game.", isWarning: true);
 
             }
