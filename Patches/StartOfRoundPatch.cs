54567eb97bfaa01e7d6d0fa21ab2f85679316c1f
zeeblo <141220959+zeeblo@users.noreply.github.com>
2024-10-28 16:51:05 -0400

    simplified horn spawn
    using built-in lethal method to spawn meeting horn

--- Patches/StartOfRoundPatch.cs
+++ Patches/StartOfRoundPatch.cs
@@ -1,4 +1,6 @@
-﻿using HarmonyLib;
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System.Reflection;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -11,40 +13,12 @@ namespace LethalMystery.Patches
     internal class StartOfRoundPatch
     {
 
-        private static void SpawnHorn(StartOfRound __instance, GameObject prefab, Vector3 pos, int unlockableIndex)
-        {
-            GameObject gameObject = UnityEngine.Object.Instantiate(prefab, pos, Quaternion.identity, null);
-
-            if (!gameObject.GetComponent<NetworkObject>().IsSpawned)
-            {
-                gameObject.GetComponent<NetworkObject>().Spawn();
-            }
-            if (gameObject != null)
-            {
-                __instance.SpawnedShipUnlockables.Add(unlockableIndex, gameObject);
-            }
-        }
-
-
         [HarmonyPatch(typeof(StartOfRound), "Start")]
         [HarmonyPostfix]
-        private static void StartPatch(StartOfRound __instance)
+        private static void SpawnHorn(StartOfRound __instance)
         {
-
-            // Spawn the meeting horn
-            for (int k = 0; k < __instance.unlockablesList.unlockables.Count; k++)
-            {
-                if (__instance.unlockablesList.unlockables[k].unlockableName.Contains("Loud"))
-                {
-
-                    SpawnHorn(
-                        __instance : __instance,
-                        prefab : __instance.unlockablesList.unlockables[k].prefabObject,
-                        pos : __instance.elevatorTransform.position,
-                        unlockableIndex : k
-                        );
-                }
-            }
+            MethodInfo SpawnUnlockable = typeof(StartOfRound).GetMethod("SpawnUnlockable", BindingFlags.NonPublic | BindingFlags.Instance);
+            SpawnUnlockable.Invoke(__instance, new object[] {18});
         }
 
 
