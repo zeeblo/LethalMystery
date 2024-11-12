610a40896df5f9cd04703c875206d21c94bb46d0
zeeblo <141220959+zeeblo@users.noreply.github.com>
2024-11-11 22:28:02 -0500

    removed comments

--- Plugin.cs
+++ Plugin.cs
@@ -12,26 +12,6 @@ using UnityEngine.SceneManagement;
 
 
 
-
-/*
- * - Add Start command
- * ! Have a GUI that says if they're the monster/crew & if they're shapeshifter (Stays on whole game)
- * - Have the speaker dialouge tell players their role instead.
- * - Have a chat commands that will tell the users their role & what they do
- * - Imposters can spawn their weapon (Using config not Keyboard)
- * - 
-*/
-
-
-
-
-/*
- * Win Game Mechanics
- * - Top text will say (Monsters/Employees won).
- * - If monsters won the bottom text will say the names of the 1 or 2 imps
-*/
-
-
 namespace LethalMystery
 {
     [BepInPlugin(modGUID, modName, modVersion)]
