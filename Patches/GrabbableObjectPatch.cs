bc8c84d1979edb08de02acb883a5248d8d1d46d2
zeeblo <141220959+zeeblo@users.noreply.github.com>
2024-11-24 02:47:23 -0500

    hide items from map

---
+++ Patches/GrabbableObjectPatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace LethalMystery.Patches
+{
+    [HarmonyPatch(typeof(GrabbableObject))]
+    internal class GrabbableObjectPatch
+    {
+
+        [HarmonyPatch(typeof(GrabbableObject), "LateUpdate")]
+        [HarmonyPostfix]
+        private static void HideWeaponOnMonitorPatch(ref Transform ___radarIcon)
+        {
+            if (___radarIcon != null)
+            {
+                ___radarIcon.gameObject.SetActive(false);
+            }
+        }
+    }
+}
