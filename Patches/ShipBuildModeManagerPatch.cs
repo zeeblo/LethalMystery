b96c4cec6efde793af0a0b4effbc3953d145d644
zeeblo <141220959+zeeblo@users.noreply.github.com>
2024-12-24 02:57:24 -0800

    Storage
    - Only allow users to store items before a round starts.

---
+++ Patches/ShipBuildModeManagerPatch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+
+namespace LethalMystery.Patches
+{
+    [HarmonyPatch(typeof(ShipBuildModeManager))]
+    internal class ShipBuildModeManagerPatch
+    {
+
+        /// <summary>
+        /// Prevent users from storing items when the game has started
+        /// </summary>
+        [HarmonyPatch(typeof(ShipBuildModeManager), nameof(ShipBuildModeManager.StoreObjectLocalClient))]
+        [HarmonyPrefix]
+        private static bool PreventStoring()
+        {
+            if (StartOfRound.Instance.inShipPhase)
+            {
+                return true;
+            }
+            HUDManager.Instance.DisplayTip("Oops!", "You can only store items before the game has started.");
+            return false;
+        }
+    }
+}
