0c2e04516eaaf04cdf8d4645c6b3d73af1f7aeab
zeeblo <141220959+zeeblo@users.noreply.github.com>
2025-01-05 16:36:30 -0800

    VotingUI Cleaned Up
    figured out transform.find was a thing, oops

--- UI/VotingUI.cs
+++ UI/VotingUI.cs
@@ -57,126 +57,41 @@ namespace LethalMystery.UI
         }
 
 
-        public static void VoteButton(GameObject parent)
+        public static void VoteButton(GameObject playerListSlot)
         {
-            // Note: Optimize / Make readable later
-
-            List<GameObject> parentOBJ = GOTools.GetAllChildren(parent);
             bool moreCompany = Plugin.FoundThisMod("me.swipez.melonloader.morecompany");
 
-            if (moreCompany == false)
-            {
-                foreach (GameObject plistClone in parentOBJ)
-                {
-                    if (plistClone.gameObject.name.ToLower() == "image")
-                    {
-                        foreach (GameObject img in GOTools.GetAllChildren(plistClone))
-                        {
-                            if (img.gameObject.name.ToLower() == "playerlistslot")
-                            {
-                                foreach (GameObject playerListSlot in GOTools.GetAllChildren(img))
-                                {
-                                    if (playerListSlot.gameObject.name.ToLower() == "kickbutton")
-                                    {
-                                        GameObject playerVoteBtn = playerListSlot;
-                                        playerVoteBtn.SetActive(true);
+            GameObject plistClone = playerListSlot.transform.Find("Image").gameObject;
+            GameObject playerVoteBtn = playerListSlot.transform.Find("Image/PlayerListSlot/KickButton").gameObject;
+            playerVoteBtn.SetActive(true);
 
-                                        GameObject skipButton = Plugin.Instantiate(playerVoteBtn);
-                                        RectTransform skipButtonRect = skipButton.GetComponent<RectTransform>();
-                                        skipButton.transform.SetParent(plistClone.transform, false);
-                                        skipButtonRect.anchoredPosition = new Vector2(-100f, -155f);
-
-        
-                                        GameObject skipText = Plugin.Instantiate(GetImageHeader(parent));
-                                        RectTransform skipTextRect = skipText.GetComponent<RectTransform>();
-                                        TextMeshProUGUI skipTextTXT = skipText.GetComponent<TextMeshProUGUI>();
-                                        skipTextTXT.fontSize = 17;
-                                        skipTextTXT.text = "SKIP";
-                                        skipText.transform.SetParent(plistClone.transform, false);
-                                        skipTextRect.anchoredPosition = new Vector2(45f, -318f);
+            GameObject skipButton = Plugin.Instantiate(playerVoteBtn);
+            RectTransform skipButtonRect = skipButton.GetComponent<RectTransform>();
+            skipButton.transform.SetParent(plistClone.transform, false);
+            skipButtonRect.anchoredPosition = new Vector2(-100f, -155f);
 
 
-                                        UnityEngine.UI.Image plrCheckSprite = playerVoteBtn.GetComponent<UnityEngine.UI.Image>();
-                                        UnityEngine.UI.Image skipCheckSprite = skipButton.GetComponent<UnityEngine.UI.Image>();
-                                        plrCheckSprite.sprite = Plugin.CheckboxEmptyIcon;
-                                        skipCheckSprite.sprite = Plugin.CheckboxEmptyIcon;
-
-                                        UnityEngine.UI.Button plrbutton = playerVoteBtn.GetComponent<UnityEngine.UI.Button>();
-                                        UnityEngine.UI.Button skipBtn = skipButton.GetComponent<UnityEngine.UI.Button>();
-
-                                        int index = 0; // use player id
-                                        plrbutton.onClick.AddListener(() => VoteButtonClick(index, plrCheckSprite));
-                                        skipBtn.onClick.AddListener(() => SkipButtonClick(index, skipCheckSprite));
-                                    }
-                                }
-
-                            }
-                        }
-                    }
-                }
-            }
-            else
-            {
-                foreach (GameObject plistClone in parentOBJ)
-                {
-                    if (plistClone.gameObject.name.ToLower() == "image")
-                    {
-                        foreach (GameObject img in GOTools.GetAllChildren(plistClone))
-                        {
-                            if (img.gameObject.name.ToLower().Contains("quickmenuoverride"))
-                            {
-                                foreach (GameObject quickMenu in GOTools.GetAllChildren(img))
-                                {
-                                    if (quickMenu.gameObject.name.ToLower() == "holder")
-                                    {
-                                        foreach (GameObject holder in GOTools.GetAllChildren(quickMenu))
-                                        {
-                                            if (holder.gameObject.name.ToLower().Contains("playerlistslot"))
-                                            {
-                                                foreach (GameObject obj in GOTools.GetAllChildren(holder))
-                                                {
-                                                    if (obj.gameObject.name.ToLower() == "kickbutton")
-                                                    {
-                                                        GameObject playerVoteBtn = obj;
-                                                        playerVoteBtn.SetActive(true);
-
-                                                        GameObject skipButton = Plugin.Instantiate(playerVoteBtn);
-                                                        RectTransform skipButtonRect = skipButton.GetComponent<RectTransform>();
-                                                        skipButton.transform.SetParent(plistClone.transform, false);
-                                                        skipButtonRect.anchoredPosition = new Vector2(-100f, -155f);
+            GameObject skipText = Plugin.Instantiate(GetImageHeader(playerListSlot));
+            RectTransform skipTextRect = skipText.GetComponent<RectTransform>();
+            TextMeshProUGUI skipTextTXT = skipText.GetComponent<TextMeshProUGUI>();
+            skipTextTXT.fontSize = 17;
+            skipTextTXT.text = "SKIP";
+            skipText.transform.SetParent(plistClone.transform, false);
+            skipTextRect.anchoredPosition = new Vector2(45f, -318f);
 
 
-                                                        GameObject skipText = Plugin.Instantiate(GetImageHeader(parent));
-                                                        RectTransform skipTextRect = skipText.GetComponent<RectTransform>();
-                                                        TextMeshProUGUI skipTextTXT = skipText.GetComponent<TextMeshProUGUI>();
-                                                        skipTextTXT.fontSize = 17;
-                                                        skipTextTXT.text = "SKIP";
-                                                        skipText.transform.SetParent(plistClone.transform, false);
-                                                        skipTextRect.anchoredPosition = new Vector2(45f, -318f);
+            UnityEngine.UI.Image plrCheckSprite = playerVoteBtn.GetComponent<UnityEngine.UI.Image>();
+            UnityEngine.UI.Image skipCheckSprite = skipButton.GetComponent<UnityEngine.UI.Image>();
+            plrCheckSprite.sprite = Plugin.CheckboxEmptyIcon;
+            skipCheckSprite.sprite = Plugin.CheckboxEmptyIcon;
 
+            UnityEngine.UI.Button plrbutton = playerVoteBtn.GetComponent<UnityEngine.UI.Button>();
+            UnityEngine.UI.Button skipBtn = skipButton.GetComponent<UnityEngine.UI.Button>();
 
-                                                        UnityEngine.UI.Image plrCheckSprite = playerVoteBtn.GetComponent<UnityEngine.UI.Image>();
-                                                        UnityEngine.UI.Image skipCheckSprite = skipButton.GetComponent<UnityEngine.UI.Image>();
-                                                        plrCheckSprite.sprite = Plugin.CheckboxEmptyIcon;
-                                                        skipCheckSprite.sprite = Plugin.CheckboxEmptyIcon;
+            int index = 0; // use player id
+            plrbutton.onClick.AddListener(() => VoteButtonClick(index, plrCheckSprite));
+            skipBtn.onClick.AddListener(() => SkipButtonClick(index, skipCheckSprite));
 
-                                                        UnityEngine.UI.Button plrbutton = playerVoteBtn.GetComponent<UnityEngine.UI.Button>();
-                                                        UnityEngine.UI.Button skipBtn = skipButton.GetComponent<UnityEngine.UI.Button>();
-
-                                                        int index = 0; // use player id
-                                                        plrbutton.onClick.AddListener(() => VoteButtonClick(index, plrCheckSprite));
-                                                        skipBtn.onClick.AddListener(() => SkipButtonClick(index, skipCheckSprite));
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
 
 
         }
@@ -195,87 +110,19 @@ namespace LethalMystery.UI
         }
 
 
-        private static void ShowVotesForPlayers(GameObject parent)
+        private static void ShowVotesForPlayers(GameObject playerListSlot)
         {
-            // Note: Optimize / Make readable later
-
-            List<GameObject> parentOBJ = GOTools.GetAllChildren(parent);
             string votes = "Votes: ";
             bool moreCompany = Plugin.FoundThisMod("me.swipez.melonloader.morecompany");
 
-            if (moreCompany == false)
-            {
-                foreach (GameObject plistClone in parentOBJ)
-                {
-                    if (plistClone.gameObject.name.ToLower() == "image")
-                    {
-                        foreach (GameObject img in GOTools.GetAllChildren(plistClone))
-                        {
-                            if (img.gameObject.name.ToLower() == "playerlistslot")
-                            {
-                                foreach (GameObject playerListSlot in GOTools.GetAllChildren(img))
-                                {
-                                    if (playerListSlot.gameObject.name.ToLower() == "voicevolumeslider")
-                                    {
-                                        playerListSlot.SetActive(true);
-                                        foreach (GameObject voiceSlider in GOTools.GetAllChildren(playerListSlot))
-                                        {
-                                            if (voiceSlider.gameObject.name.ToLower() == "image" || voiceSlider.gameObject.name.ToLower() == "slider")
-                                            {
-                                                voiceSlider.SetActive(false);
-                                            }
-                                            if (voiceSlider.gameObject.name.ToLower() == "text (1)")
-                                            {
-                                                voiceSlider.SetActive(true);
-                                                voiceSlider.gameObject.GetComponent<TextMeshProUGUI>().text = votes;
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            else
-            {
-                foreach (GameObject plistClone in parentOBJ)
-                {
-                    if (plistClone.gameObject.name.ToLower() == "image")
-                    {
-                        foreach (GameObject img in GOTools.GetAllChildren(plistClone))
-                        {
-                            if (img.gameObject.name.ToLower().Contains("quickmenuoverride"))
-                            {
-                                foreach (GameObject quickMenu in GOTools.GetAllChildren(img))
-                                {
-                                    if (quickMenu.gameObject.name.ToLower() == "holder")
-                                    {
-                                        foreach (GameObject holder in GOTools.GetAllChildren(quickMenu))
-                                        {
-                                            if (holder.gameObject.name.ToLower().Contains("playerlistslot"))
-                                            {
-                                                foreach (GameObject obj in GOTools.GetAllChildren(holder))
-                                                {
-                                                    if (obj.gameObject.name.ToLower() == "voicevolumeslider")
-                                                    {
-                                                        obj.SetActive(false);
-                                                    }
-                                                    if (obj.gameObject.name.ToLower() == "text (1)")
-                                                    {
-                                                        obj.GetComponent<TextMeshProUGUI>().text = votes;
-                                                        obj.SetActive(true);
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+            GameObject VolSlider = playerListSlot.transform.Find("Image/PlayerListSlot/VoiceVolumeSlider").gameObject;
+            GameObject playerVoteObj = playerListSlot.transform.Find("Image/PlayerListSlot/VoiceVolumeSlider/Text (1)").gameObject;
+            VolSlider.SetActive(true);
+            VolSlider.transform.Find("Image").gameObject.SetActive(false);
+            VolSlider.transform.Find("Slider").gameObject.SetActive(false);
+
+            playerVoteObj.SetActive(true);
+            playerVoteObj.gameObject.GetComponent<TextMeshProUGUI>().text = votes;
 
 
         }
