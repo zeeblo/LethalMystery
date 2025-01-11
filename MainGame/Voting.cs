3416778605befaf56b6afa645e461344441a9de2
zeeblo <141220959+zeeblo@users.noreply.github.com>
2025-01-11 13:26:04 -0800

    Votelist: removed unused patch

--- MainGame/Voting.cs
+++ MainGame/Voting.cs
@@ -50,24 +50,6 @@ namespace LethalMystery.MainGame
             Plugin.netHandler.playerDiedReceive($"{Plugin.localPlayer.playerClientId}", Plugin.localPlayer.playerClientId);
         }
 
-        /*
-        [HarmonyPatch(typeof(PlayerControllerB), nameof(PlayerControllerB.KillPlayerClientRpc))]
-        [HarmonyPostfix]
-        private static void NotifyPlayerDeathToUsers(int playerId)
-        {
-            if (Plugin.localPlayer.IsHost)
-            {
-                // Because apparently the host will execute this method
-                // multiple times
-                return;
-            }
-
-            Plugin.mls.LogInfo($">>> in KillPlayerClientRpc: {playerId} ");
-            amountOfPlayers -= 1;
-            RefreshPlayerVotes($"{playerId}");
-            Plugin.netHandler.playerDiedReceive($"{playerId}", Plugin.localPlayer.playerClientId);
-        }
-        */
         #endregion Patches
 
 
