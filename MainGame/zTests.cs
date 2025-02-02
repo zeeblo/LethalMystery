5c6a710e0a831f171a293e5d346170bdbba385da
zeeblo <141220959+zeeblo@users.noreply.github.com>
2025-02-02 15:27:29 -0800

    wip interior stuff

--- MainGame/zTests.cs
+++ MainGame/zTests.cs
@@ -38,11 +38,40 @@ namespace LethalMystery.MainGame
 
 
 
-        [HarmonyPatch]
+        [HarmonyPatch(typeof(Terminal))]
         internal class AdminCMDS
         {
 
-
+            [HarmonyPatch(nameof(Terminal.Update))]
+            [HarmonyPostfix]
+            private static void UpdatePatch(Terminal __instance)
+            {
+                if (Keyboard.current.digit2Key.wasPressedThisFrame)
+                {
+                    Plugin.mls.LogInfo(">>> Pressed 2");
+                    foreach (TerminalNode twords in __instance.terminalNodes.specialNodes)
+                    {
+                            Plugin.mls.LogInfo($">>> displayText: {twords.displayText}");
+                            Plugin.mls.LogInfo($">>> terminalEvent: {twords.terminalEvent}");
+                            Plugin.mls.LogInfo($">>> clearPreviousText: {twords.clearPreviousText}");
+                            Plugin.mls.LogInfo($">>> maxCharactersToType: {twords.maxCharactersToType}");
+                            Plugin.mls.LogInfo($">>> buyVehicleIndex: {twords.buyVehicleIndex}");
+                            Plugin.mls.LogInfo($">>> isConfirmationNode: {twords.isConfirmationNode}");
+                            Plugin.mls.LogInfo($">>> buyRerouteToMoon: {twords.buyRerouteToMoon}");
+                            Plugin.mls.LogInfo($">>> displayPlanetInfo: {twords.displayPlanetInfo}");
+                            Plugin.mls.LogInfo($">>> shipUnlockableID: {twords.shipUnlockableID}");
+                            Plugin.mls.LogInfo($">>> buyUnlockable: {twords.buyUnlockable}");
+                            Plugin.mls.LogInfo($">>> returnFromStorage: {twords.returnFromStorage}");
+                            Plugin.mls.LogInfo($">>> itemCost: {twords.itemCost}");
+                            Plugin.mls.LogInfo($">>> creatureFileID: {twords.creatureFileID}");
+                            Plugin.mls.LogInfo($">>> creatureName: {twords.creatureName}");
+                            Plugin.mls.LogInfo($">>> storyLogFileID: {twords.storyLogFileID}");
+                            Plugin.mls.LogInfo($">>> overrideOptions: {twords.overrideOptions}");
+                            Plugin.mls.LogInfo($">>> acceptAnything: {twords.acceptAnything}");
+                        
+                    }
+                }
+            }
         }
 
 
@@ -94,6 +123,7 @@ namespace LethalMystery.MainGame
                     
                 }
 
+                /*
                 if (Keyboard.current.digit2Key.wasPressedThisFrame)
                 {
                     
@@ -105,7 +135,7 @@ namespace LethalMystery.MainGame
                     Plugin.mls.LogInfo($"Active Render Pipeline: {pipline}");
                     Plugin.mls.LogInfo($"Skeld Pipeline: {skeldPip}");
                 }
-
+                */
             }
         }
     }
--- Maps/CustomLvl.cs
+++ Maps/CustomLvl.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using HarmonyLib;
+﻿using HarmonyLib;
+using LethalMystery.Utils;
 
 
 namespace LethalMystery.Maps
@@ -10,20 +8,144 @@ namespace LethalMystery.Maps
     [HarmonyPatch]
     internal class CustomLvl
     {
+        public static List<SelectableLevel> customMoons = new List<SelectableLevel>();
+        public static List<TerminalKeyword> customKeywords = new List<TerminalKeyword>();
+        public static List<TerminalNode> customNodes = new List<TerminalNode>();
+
+        private class NewSelectableLevel : SelectableLevel
+        {
+
+            public NewSelectableLevel(string planetname)
+            {
+                PlanetName = planetname;
+            }
+
+        }
+
+
+        
+
+        private class NewTerminalKeyword : TerminalKeyword
+        {
+
+            public NewTerminalKeyword(string word, bool isVerb, TerminalNode specialKeywordResult)
+            {
+                this.word = word;
+                this.isVerb = isVerb;
+                this.specialKeywordResult = specialKeywordResult;
+            }
+
+        }
+        
+
+
+        private class NewTerminalNode : TerminalNode
+        {
+            public NewTerminalNode(
+                string displayText = "",
+                bool clearPreviousText = true,
+                int maxCharactersToType = 25,
+                int buyItemIndex = -1,
+                int buyVehicleIndex = -1,
+                bool isConfirmationNode = false,
+                int buyRerouteToMoon = -1,
+                int displayPlanetInfo = -1,
+                int shipUnlockableID = -1,
+                int itemCost = 0,
+                bool buyUnlockable = false,
+                bool returnFromStorage = false)
+            {
+                this.displayText = displayText;
+                this.clearPreviousText = clearPreviousText;
+                this.maxCharactersToType = maxCharactersToType;
+                this.buyItemIndex = buyItemIndex;
+                this.buyVehicleIndex = buyVehicleIndex;
+                this.isConfirmationNode = isConfirmationNode;
+                this.buyRerouteToMoon = buyRerouteToMoon;
+                this.displayPlanetInfo = displayPlanetInfo;
+                this.shipUnlockableID = shipUnlockableID;
+                this.itemCost = itemCost;
+                this.buyUnlockable = buyUnlockable;
+                this.returnFromStorage = returnFromStorage;
+            }
+        }
+
+
+
+
+        private static void AppendNewSelectableLevel()
+        {
+            customMoons.Add(new NewSelectableLevel("skeld"));
+        }
+
+        private static void AppendNewTerminalKeyword()
+        {
+            customKeywords.Add(new NewTerminalKeyword("skeld", false, new NewTerminalNode("Interior will now be Skeld\n\n")));
+        }
+
+        private static void AppendNewTerminalNode()
+        {
+            customNodes.Add(new NewTerminalNode());
+        }
+
+
+
+
 
 
         [HarmonyPatch(typeof(Terminal), nameof(Terminal.Awake))]
         [HarmonyPrefix]
-        private static void MoonsInTerminal(Terminal __instance)
+        private static bool MoonsInTerminal(Terminal __instance)
         {
-            List<SelectableLevel> allMoons = new List<SelectableLevel>();
+            customMoons.Clear();
+            customKeywords.Clear();
+            customNodes.Clear();
+            AppendNewSelectableLevel();
+            AppendNewTerminalKeyword();
+            AppendNewTerminalNode();
+            
 
-
-
-            foreach (SelectableLevel moon in __instance.moonsCatalogueList)
+            foreach (TerminalKeyword words in __instance.terminalNodes.allKeywords)
             {
-                allMoons.Add(moon);
+                customKeywords.Add(words);
             }
+
+            /*
+            foreach (TerminalNode node in __instance.terminalNodes.specialNodes)
+            {
+                customKeywords.Add(words);
+            }
+            */
+            __instance.moonsCatalogueList = customMoons.ToArray();
+            __instance.terminalNodes.allKeywords = customKeywords.ToArray();
+
+            return true;
         }
+
+
+        [HarmonyPatch(typeof(Terminal), nameof(Terminal.BeginUsingTerminal))]
+        [HarmonyPrefix]
+        private static void StartPatch(Terminal __instance)
+        {
+
+
+            foreach (TerminalKeyword twords in __instance.terminalNodes.allKeywords)
+            {
+                if (twords.word == "moons")
+                {
+                    string raw_result = twords.specialKeywordResult.displayText.Substring(0, 219);
+                    string result = "";
+                    foreach (SelectableLevel moon in __instance.moonsCatalogueList)
+                    {
+                        result += raw_result + $"\n* {StringAddons.Title(moon.PlanetName)} [planetTime]\n\n";
+                    }
+
+                    twords.specialKeywordResult.displayText = result;
+                    break;
+                }
+            }
+
+        }
+
     }
 }
